59cc7079554325086104c7f566ade447318123b6
Shi Yu <shiyuwudi@gmail.com>
2020-12-07 15:11:15 +0900

    1time

--- routes/qrcode.js
+++ routes/qrcode.js
@@ -5,7 +5,9 @@ const { FamiCodes } = require('../db/db2');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   (async () => {
+    console.log(111);
     const data = await FamiCodes.getCode()
+    console.log(222, data);
     res.send({
       success: true,
       data,
